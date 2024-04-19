6c7a818565e84cac95fc5deafaa19ee6aa304d1e
habibmimouna <habibmimouna00@gmail.com>
2024-04-19 04:41:46 +0100

    complete calendar component

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -25,10 +25,10 @@ export class CalendarComponent {
 
   reservation: Reservation = {
     day: this.selectedDay,
-    reservedHours: [], //this.selectedHours,
+    reservedHours: this.selectedHours,
     purpose: '',
-    meetingRoom: '', //this.meetingRoomId,
-    user: '', //this.getUserIdFromToken(),
+    meetingRoom: this.meetingRoomId,
+    user: this.getUserIdFromToken(),
   };
 
   constructor(
@@ -47,7 +47,7 @@ export class CalendarComponent {
     });
     console.log(this.meetingRoomId);
 
-    //this.generateHoursOfDay();
+    
   }
 
   generateDaysInMonth(): void {
@@ -69,12 +69,13 @@ export class CalendarComponent {
     this.selectedMonth = newDate.getMonth() + 1;
     this.generateDaysInMonth();
   }
-  async selectDay(day: Date): Promise<void> {
+   selectDay(day: Date): void {
     const month = (day.getMonth() + 1).toString().padStart(2, '0');
     const dayOfMonth = day.getDate().toString().padStart(2, '0');
     const year = day.getFullYear();
     const formattedDate = `${month}-${dayOfMonth}-${year}`;
     this.selectedDay = formattedDate;
+    this.reservation.day=formattedDate;
     console.log(this.selectedDay, this.meetingRoomId);
     this.getAvailableHours();
   }
@@ -111,8 +112,48 @@ export class CalendarComponent {
     this.updateCheckedStatus();
     console.log(this.selectedHours);
     
+    
   }
   updateCheckedStatus(): void {
     this.hasCheckedHours = this.selectedHours.length > 0;
   }
+
+  getUserIdFromToken(): string | null {
+    const currentUser = localStorage.getItem('currentUser');
+    if (currentUser) {
+      try {
+        const userObject = JSON.parse(currentUser);
+        if (userObject && userObject._id) {
+          return userObject._id;
+        } else {
+          console.error('User object does not contain _id field.');
+          return null;
+        }
+      } catch (error) {
+        console.error('Error parsing currentUser JSON:', error);
+        return null;
+      }
+    } else {
+      console.error('No currentUser found in localStorage.');
+      return null;
+    }
+  }
+  async makeReservation(){
+    console.log(this.reservation);
+    try {
+      await this.reservationService.createReservation(this.reservation).subscribe((data)=>{
+        console.error('reservation made with success:', data);
+        window.location.reload()
+
+      }, (error) => {
+        // Handle error
+        console.error('Error making reservation:', error);
+        // You can also notify the user about the error, show a toast message, etc.
+      });
+    } catch (error) {
+      console.error('Error making reservation:', error);
+      // Handle any unexpected errors here
+    }
+  }
+
 }
--- src/app/services/reservation.service.ts
+++ src/app/services/reservation.service.ts
@@ -1,34 +1,34 @@
 import { Injectable } from '@angular/core';
-import { MeetingRoom } from '../models/meetingRoom';
-import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Reservation } from '../models/reservation';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ReservationService {
-  private baseURL = 'http://localhost:5000/meeting-Rooms/meeting-rooms';
+  private baseURL = 'http://localhost:5000/reservations/reservations';
 
   constructor(private httpClient: HttpClient) {}
-  createMeetingRoom(meetingRoom:MeetingRoom): Observable<Object> {
+
+  getReservationList(): Observable<Reservation[]> {
+    return this.httpClient.get<Reservation[]>(`${this.baseURL}`);
+  }
+
+  createReservation(reservation: Reservation): Observable<Object> {
     let currentToken = localStorage.getItem('userToken') || "";
     const headers= new HttpHeaders().set('Authorization',currentToken);
-    return this.httpClient.post(this.baseURL, meetingRoom, { headers: headers });
+    return this.httpClient.post(`${this.baseURL}`, reservation, { headers: headers });
   }
 
-  deleteMeetingRoom(id: string): Observable<Object> {
-    return this.httpClient.delete(`${this.baseURL}/${id}`);
+  deleteReservation(res:Reservation): Observable<Object> {
+    return this.httpClient.delete(this.baseURL, { body: { res } });
   }
-
-  getMeetingRoomById(id: string): Observable<MeetingRoom> {
-    return this.httpClient.get<MeetingRoom>(`${this.baseURL}/${id}`);
+  updateReservation(id:string): Observable<Object> {
+    let currentToken = localStorage.getItem('userToken') || '';
+    const headers = new HttpHeaders().set('Authorization', currentToken);
+    return this.httpClient.put(`${this.baseURL}`, id, {
+      headers: headers,
+    });
   }
-  getMeetingRoomList(): Observable<MeetingRoom[]> {
-    return this.httpClient.get<MeetingRoom[]>(`${this.baseURL}`);
-  }
-
-  updateMeetingRoom(id: number, meetingRoom: MeetingRoom): Observable<Object> {
-    return this.httpClient.put(`${this.baseURL}/${id}`, meetingRoom);
-  }
-  
 }
